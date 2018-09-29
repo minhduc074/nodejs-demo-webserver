0f24d0bc59f04f35b4952455ff6896a0de8f3f10
minhduc074 <minhduc074@gmail.com>
2018-09-29 10:02:36 +0700

    update http listen

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var express = require('express');
 
 var app = express();
 var http = require("http").createServer(app);
-http.listen(3000);
+http.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.get("/", function(req, res) {
     console.log("hello");
